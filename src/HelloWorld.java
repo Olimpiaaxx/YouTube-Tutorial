f71b2d2d0ed48c499565e956a7107cca17dff4c2
Olimpiaaxx <olimpiapater@gmail.com>
2021-01-27 18:02:02 +0000

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class HelloWorld {
+    static Scanner sc = new Scanner(System.in);
+
+    final double SHORTPI = 3.14159;
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        int var1 = 100;
+        int v2, v3;
+
+
+    }
+}
