2871375fb688475944e16cc7eecedddd5fb93b44
Olimpiaaxx <olimpiapater@gmail.com>
2021-03-16 17:43:42 +0000

    Learning Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,35 +7,50 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        int[] a1 = new int[10];
-        a1[0] = 1;
-        Arrays.fill(a1,2);
-        System.out.println(a1[0]);
-        System.out.println(a1.length);
-        String[] a2 = {"one", "two"};
-        int[] oneTo10 = IntStream.rangeClosed(1,10).toArray();
+            ArrayList<String> aL1 = new ArrayList<String>(20);
+            aL1.add("Sue");
+            ArrayList<Integer> aL2 = new ArrayList<>(Arrays.asList(1,2,3,4));
+            for(Integer x: aL2) System.out.print(x);
 
-        for(int x: oneTo10) System.out.println(x);
 
-        System.out.println(Arrays.binarySearch(oneTo10, 9));
 
-        int a3 [][] = new int[2][2];
-        String[][] a4 = {{"00", "10"}, {"01", "11"}};
 
-        System.out.println(a4[1][1]);
 
-        String a5[][][] = {{{"000"}, {"100"}, {"200"}, {"300"}},
-                {{"010"}, {"110"}, {"210"}, {"310"}},
-                {{"020"}, {"120"}, {"220"}, {"320"}}};
 
-        System.out.println(a5[2][3][0]);
 
-        int a6[] = {1,2,3};
-        int a7[] = Arrays.copyOf(a6, 3);
-        System.out.println(Arrays.equals(a6, a7));
-        int a8[] = {3,2,1};
-        Arrays.sort(a8);
-        System.out.println(Arrays.toString(a8));
+
+
+
+
+       // int[] a1 = new int[10];
+       // a1[0] = 1;
+       // Arrays.fill(a1,2);
+       // System.out.println(a1[0]);
+       // System.out.println(a1.length);
+        //String[] a2 = {"one", "two"};
+        //int[] oneTo10 = IntStream.rangeClosed(1,10).toArray();
+
+       // for(int x: oneTo10) System.out.println(x);
+
+       // System.out.println(Arrays.binarySearch(oneTo10, 9));
+
+       // int a3 [][] = new int[2][2];
+       // String[][] a4 = {{"00", "10"}, {"01", "11"}};
+
+       // System.out.println(a4[1][1]);
+
+       // String a5[][][] = {{{"000"}, {"100"}, {"200"}, {"300"}},
+       //         {{"010"}, {"110"}, {"210"}, {"310"}},
+        //        {{"020"}, {"120"}, {"220"}, {"320"}}};
+
+      //  System.out.println(a5[2][3][0]);
+
+       // int a6[] = {1,2,3};
+       // int a7[] = Arrays.copyOf(a6, 3);
+       // System.out.println(Arrays.equals(a6, a7));
+       // int a8[] = {3,2,1};
+       // Arrays.sort(a8);
+       // System.out.println(Arrays.toString(a8));
 
 
 
